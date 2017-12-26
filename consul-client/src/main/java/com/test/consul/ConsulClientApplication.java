947664fdf9033112329527a957959922e66cf1dc
Xu Yao <xuyao@thoughtworks.com>
2017-12-26 10:06:51 +0800

    consul server & client

---
+++ consul-client/src/main/java/com/test/consul/ConsulClientApplication.java
@@ -0,0 +1,13 @@
+package com.test.consul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulClientApplication.class);
+    }
+}
