c2fa81d9219c12b77361b77b40eda0627bb32604
Xu Yao <xuyao@thoughtworks.com>
2018-01-08 20:41:05 +0800

    add swagger for service order

--- service-order/src/main/java/com/test/controller/ShopController.java
+++ service-order/src/main/java/com/test/controller/ShopController.java
@@ -3,6 +3,8 @@ package com.test.controller;
 import com.test.entity.ShopEntity;
 import com.test.model.Shops;
 import com.test.service.ShopService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/v1")
+@Api(value = "Shop controller")
 public class ShopController {
     private ShopService shopService;
 
@@ -21,6 +24,7 @@ public class ShopController {
         this.shopService = shopService;
     }
 
+    @ApiOperation(value = "retrieve all shops", response = Shops.class)
     @GetMapping("/shops")
     public Shops listAllShops(@PageableDefault Pageable pageable, PagedResourcesAssembler<ShopEntity> assembler) {
         return shopService.retrieveAllShops(pageable, assembler);
