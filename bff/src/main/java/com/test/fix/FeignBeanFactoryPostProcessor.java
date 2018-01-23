48818d38841da8ec4484cc169b9ef191d3adffa2
Xu Yao <xuyao@thoughtworks.com>
2018-01-23 17:22:18 +0800

    init config server

---
+++ bff/src/main/java/com/test/fix/FeignBeanFactoryPostProcessor.java
@@ -0,0 +1,28 @@
+package com.test.fix;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * https://github.com/spring-cloud/spring-cloud-netflix/issues/1952
+ */
+@Component
+public class FeignBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        if (containsBeanDefinition(beanFactory, "feignContext", "eurekaAutoServiceRegistration")) {
+            BeanDefinition bd = beanFactory.getBeanDefinition("feignContext");
+            bd.setDependsOn("eurekaAutoServiceRegistration");
+        }
+    }
+
+    private boolean containsBeanDefinition(ConfigurableListableBeanFactory beanFactory, String... beans) {
+        return Arrays.stream(beans).allMatch(beanFactory::containsBeanDefinition);
+    }
+}
