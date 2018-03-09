e0aa125bedeccdc67e68c80755dc7b4eaeb29d35
Xu Yao <xuyao@thoughtworks.com>
2018-03-09 17:35:27 +0800

    init api-utils

--- service-order/src/main/java/com/test/Application.java
+++ service-order/src/main/java/com/test/Application.java
@@ -2,9 +2,7 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
